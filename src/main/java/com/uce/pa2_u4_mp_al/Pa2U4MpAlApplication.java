18f7bf271e2359ac9f5690ca638d8d3ec25c41af
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-08-14 19:50:55 -0500

    taller38

---
+++ src/main/java/com/uce/pa2_u4_mp_al/Pa2U4MpAlApplication.java
@@ -0,0 +1,13 @@
+package com.uce.pa2_u4_mp_al;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U4MpAlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U4MpAlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/pa2_u4_mp_al/Pa2U4MpAlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.pa2_u4_mp_al;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa2U4MpAlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
